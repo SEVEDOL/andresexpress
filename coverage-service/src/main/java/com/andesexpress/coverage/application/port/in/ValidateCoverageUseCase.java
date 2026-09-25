d38dd96a3d0a93f1c036ffcedbf183aa98d6f68f
Juan Salamanca Ruiz <119972850+Juansr09@users.noreply.github.com>
2026-09-24 22:32:42 -0500

    Delete coverage-service directory

--- coverage-service/src/main/java/com/andesexpress/coverage/application/port/in/ValidateCoverageUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.andesexpress.coverage.application.port.in;
-
-import com.andesexpress.coverage.domain.model.TariffDomain;
-
-public interface ValidateCoverageUseCase {
-    TariffDomain validateAndCalculate(String originCity, String destinationCity, Double weight);
-}
--- coverage-service/src/main/java/com/andesexpress/coverage/application/port/out/ApiColombiaPort.java
+++
@@ -1,8 +0,0 @@
-package com.andesexpress.coverage.application.port.out;
-
-import com.andesexpress.coverage.domain.model.CityData;
-import java.util.Optional;
-
-public interface ApiColombiaPort {
-    Optional<CityData> fetchCityByName(String cityName);
-}
